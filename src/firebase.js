6ad5903f6d2afb9847dd765a2e3c3dcbab32e77f
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-12 17:21:45 +0530

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBXrWY7bbePBkza9_PGVOovpfIiYcWZq20',
-  authDomain: 'soulipie.firebaseapp.com',
-  projectId: 'soulipie',
-  storageBucket: 'soulipie.appspot.com',
-  messagingSenderId: '201876828788',
-  appId: '1:201876828788:web:ff6f7c66e3598aa17f9721'
+  apiKey: 'AIzaSyAu5znO8wlIzjpfQ10XjKzLxcx7I_evIwY',
+  authDomain: 'phone-login-test-4769a.firebaseapp.com',
+  projectId: 'phone-login-test-4769a',
+  storageBucket: 'phone-login-test-4769a.appspot.com',
+  messagingSenderId: '62106832589',
+  appId: '1:62106832589:web:c2d0decc9d1c6b1891a384'
 };
 
 // Initialize Firebase
