1c021911a1f4cbe7de0bbe1ff5e128744c66c948
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-12 18:19:09 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import './style.css';
-import { app } from './firebase';
-import { getAuth } from 'firebase/auth';
-import { signInWithPhoneNumber, RecaptchaVerifier } from 'firebase/auth';
+import React from "react";
+import "./style.css";
+import { app } from "./firebase";
+import { getAuth } from "firebase/auth";
+import { signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
 
 const auth = getAuth(app);
-// function handleChange(e) {
-//   //const { name, value } = e.target;
-//   // this.setState({
-//   //   [name]: value,
-//   // });
-//   return;
-// }
+
 const configureCaptcha = function () {
-  auth.languageCode = 'in';
+  auth.languageCode = "in";
   window.recaptchaVerifier = new RecaptchaVerifier(
-    'sign-in-button',
+    "sign-in-button",
     {
-      size: 'invisible',
+      size: "invisible",
       callback: (response) => {
         // reCAPTCHA solved, allow signInWithPhoneNumber.
         onSignInSubmit();
@@ -31,7 +25,7 @@ const onSignInSubmit = function (e) {
   e.preventDefault();
   configureCaptcha();
   //const phoneNumber = "+91" + state.mobile;
-  const phoneNumber = '+918971044793';
+  const phoneNumber = "+918971044793";
   console.log(phoneNumber);
   const appVerifier = window.recaptchaVerifier;
 
@@ -40,26 +34,26 @@ const onSignInSubmit = function (e) {
       // SMS sent. Prompt user to type the code from the message, then sign the
       // user in with confirmationResult.confirm(code).
       window.confirmationResult = confirmationResult;
-      console.log('OTP has been sent');
+      console.log("OTP has been sent");
       // ...
     })
     .catch((error) => {
       // Error; SMS not sent
       // ...
-      console.log('SMS not sent');
+      console.log("SMS not sent");
     });
 };
 const onSubmitOTP = function (e) {
   e.preventDefault();
   //const code = this.state.otp;
-  const code = '123456';
+  const code = "123456";
   console.log(code);
   confirmationResult
     .confirm(code)
     .then((result) => {
       // User signed in successfully.
       const user = result.user;
-      console.log('logged in');
+      console.log("logged in");
     })
     .catch((error) => {
       // User couldn"t sign in (bad verification code?)
