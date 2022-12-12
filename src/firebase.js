73b124e26f153bb4174a5d86f8ae9b0413272f58
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-12 17:48:37 +0530

    1 file modified

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 // const firebaseConfig = {
-//   apiKey: 'AIzaSyAu5znO8wlIzjpfQ10XjKzLxcx7I_evIwY',
-//   authDomain: 'phone-login-test-4769a.firebaseapp.com',
-//   projectId: 'phone-login-test-4769a',
-//   storageBucket: 'phone-login-test-4769a.appspot.com',
-//   messagingSenderId: '62106832589',
-//   appId: '1:62106832589:web:c2d0decc9d1c6b1891a384'
+//   apiKey: "AIzaSyAu5znO8wlIzjpfQ10XjKzLxcx7I_evIwY",
+//   authDomain: "phone-login-test-4769a.firebaseapp.com",
+//   projectId: "phone-login-test-4769a",
+//   storageBucket: "phone-login-test-4769a.appspot.com",
+//   messagingSenderId: "62106832589",
+//   appId: "1:62106832589:web:c2d0decc9d1c6b1891a384"
 // }
 
 export const app = initializeApp({
-  apiKey: 'AIzaSyAu5znO8wlIzjpfQ10XjKzLxcx7I_evIwY',
-  authDomain: 'phone-login-test-4769a.firebaseapp.com',
-  projectId: 'phone-login-test-4769a',
-  storageBucket: 'phone-login-test-4769a.appspot.com',
-  messagingSenderId: '62106832589',
-  appId: '1:62106832589:web:c2d0decc9d1c6b1891a384',
+  apiKey: "AIzaSyAu5znO8wlIzjpfQ10XjKzLxcx7I_evIwY",
+  authDomain: "phone-login-test-4769a.firebaseapp.com",
+  projectId: "phone-login-test-4769a",
+  storageBucket: "phone-login-test-4769a.appspot.com",
+  messagingSenderId: "62106832589",
+  appId: "1:62106832589:web:c2d0decc9d1c6b1891a384",
 });
