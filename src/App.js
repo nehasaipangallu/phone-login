99c6b79606a45c408f320697c78d110602e8fb81
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-12 17:12:31 +0530

    added login logic

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import firebase from './firebase';
+import './style.css';
+import { auth } from './firebase';
+import { useNavigate } from 'react-router-dom';
+import { signInWithPhoneNumber, RecaptchaVerifier } from 'firebase/auth';
 
-class App extends React.Component {
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-  configureCaptcha = () => {
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+export default function Login() {
+  let navigate = useNavigate();
+  function handleChange(e) {
+    //const { name, value } = e.target;
+    // this.setState({
+    //   [name]: value,
+    // });
+    return;
+  }
+  function configureCaptcha() {
+    auth.languageCode = 'in';
+    window.recaptchaVerifier = new RecaptchaVerifier(
       'sign-in-button',
       {
         size: 'invisible',
         callback: (response) => {
           // reCAPTCHA solved, allow signInWithPhoneNumber.
-          this.onSignInSubmit();
-          console.log('recaptcha verified');
+          onSignInSubmit();
         },
-        defaultCountry: 'IN',
-      }
+      },
+      auth
     );
-  };
-  onSignInSubmit = (e) => {
+  }
+
+  function onSignInSubmit(e) {
     e.preventDefault();
-    this.configureCaptcha();
-    const phoneNumber = '+91' + this.state.mobile;
+    configureCaptcha();
+    //const phoneNumber = '+91' + state.mobile;
+    const phoneNumber = '+918971044793';
     console.log(phoneNumber);
     const appVerifier = window.recaptchaVerifier;
-    firebase
-      .auth()
-      .signInWithPhoneNumber(phoneNumber, appVerifier)
+
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
       .then((confirmationResult) => {
         // SMS sent. Prompt user to type the code from the message, then sign the
         // user in with confirmationResult.confirm(code).
@@ -43,54 +49,53 @@ class App extends React.Component {
         // ...
         console.log('SMS not sent');
       });
-  };
-  onSubmitOTP = (e) => {
+  }
+  function onSubmitOTP(e) {
     e.preventDefault();
-    const code = this.state.otp;
+    //const code = this.state.otp;
+    const code = '123456';
     console.log(code);
-    window.confirmationResult
+    confirmationResult
       .confirm(code)
       .then((result) => {
         // User signed in successfully.
         const user = result.user;
-        console.log(JSON.stringify(user));
-        alert('User is verified');
-        // ...
+        navigate('/dashboard');
+        console.log('logged in');
       })
       .catch((error) => {
         // User couldn't sign in (bad verification code?)
-        // ...
       });
-  };
-  render() {
-    return (
-      <div>
+  }
+
+  return (
+    <div class="auth-container">
+      <div class="container auth-form">
         <h2>Login Form</h2>
-        <form onSubmit={this.onSignInSubmit}>
+        <form onSubmit={onSignInSubmit}>
           <div id="sign-in-button"></div>
           <input
             type="number"
             name="mobile"
             placeholder="Mobile number"
             required
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
-          <button type="submit">Submit</button>
+          <button type="submit">Send OTP</button>
         </form>
 
-        <h2>Enter OTP</h2>
-        <form onSubmit={this.onSubmitOTP}>
+        <h6>Enter OTP</h6>
+        <form onSubmit={onSubmitOTP}>
           <input
             type="number"
             name="otp"
             placeholder="OTP Number"
             required
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
-          <button type="submit">Submit</button>
+          <button type="submit">Log In</button>
         </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
-export default App;
